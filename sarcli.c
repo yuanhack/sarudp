2992402a8d35e3fda71b40afd9406e6ace688f46
Yuan Hong <yuanhack@163.com>
2015-08-17 16:39:23 +0800

    update input and echo hint

--- sarcli.c
+++ sarcli.c
@@ -7,12 +7,19 @@ sar_cli(FILE *fp, sarudpmgr_t *psar, const SA *pservaddr, socklen_t servlen)
 	ssize_t	n;
 	char	sendline[MAXLINE], recvline[MAXLINE + 1];
 
-	while (Fgets(sendline, MAXLINE, fp) != NULL) {
+    do {
+        fprintf(stdout, "---- input: "); 
+        fflush(stdout);
+        if (Fgets(sendline, MAXLINE, fp) == NULL) {
+            fprintf(stdout, "\n");
+            return ;
+        }
 
-		n = Sarudp_send_recv(psar, sendline, strlen(sendline),
-						 recvline, MAXLINE, pservaddr, servlen);
+        n = Sarudp_send_recv(psar, sendline, strlen(sendline),
+                recvline, MAXLINE, pservaddr, servlen);
 
-		recvline[n] = 0;	/* null terminate */
-		Fputs(recvline, stdout);
-	}
+        recvline[n] = 0;	/* null terminate */
+        fprintf(stdout, "---- echo : %s", recvline); 
+        fflush(stdout);
+    } while (1);
 }
